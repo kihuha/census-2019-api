a1696335e79177251c4665c2c61f6447c1f39bb2
kihuha <njihiadarius@gmail.com>
2021-03-22 01:08:08 +0300

    Configuring deployment

--- src/dbconn.ts
+++ src/dbconn.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg"
 
 const pool = new Pool({
   user: process.env.APP_DB_USER,
-  host: "db",
+  host: process.env.APP_DB_HOST,
   database: process.env.APP_DB_NAME,
   password: process.env.APP_DB_PASSWORD,
   port: 5432,
