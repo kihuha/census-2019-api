9f272eb5398e460c28904dc7dc0eb2b4f09d47b9
kihuha <njihiadarius@gmail.com>
2021-03-21 19:02:31 +0300

    Updated testing env

--- dockerfile
+++ dockerfile
@@ -4,8 +4,8 @@ WORKDIR /usr/app
 
 COPY ./package.json ./
 
-RUN npm install
+RUN yarn
 
 COPY ./ ./
 
-CMD ["npm", "start"]
+CMD ["yarn", "start"]
--- src/test/counties.test.ts
+++ src/test/counties.test.ts
@@ -3,5 +3,7 @@ import app from "../app"
 
 test("Should fetch specific county data", async () => {
   // lowercase
-  request(app).get("/api/v1/counties/nairobi").expect(200)
+  const res = await request(app).get("/api/v1/counties/nairobi").expect(200)
+
+  expect(res.body).toMatchObject({})
 })
