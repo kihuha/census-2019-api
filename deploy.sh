851977f3d5f26ecd878bbf991eee685528ecdf6d
kihuha <njihiadarius@gmail.com>
2021-03-23 08:31:34 +0300

    Updated CI/CD pipeline

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,7 @@
 docker build -t registry.heroku.com/census-api-kihuha/web .
 
 # PUSH IMAGE TO REGISTRY
-# b4c75131-f8e3-4f67-aebe-e84e7e36e505   ---- HEROKU OAUTH TOKEN 
+docker push registry.heroku.com/census-api-kihuha/web
+
 # RELEASE
+heroku container:release web
