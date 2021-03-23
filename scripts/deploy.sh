56b98b2521a8284b61fdac9f845c4f46c99f73f7
kihuha <njihiadarius@gmail.com>
2021-03-23 09:33:44 +0300

    Updated travis configuration

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,12 @@
 #!  /bin/bash
 
+heroku container:login
+
 # BUILD IMAGE
 docker build -t registry.heroku.com/census-api-kihuha/web .
 
 # PUSH IMAGE TO REGISTRY
-docker push registry.heroku.com/census-api-kihuha/web
+heroku container:push web -a census-api-kihuha
 
 # RELEASE
 heroku container:release web
