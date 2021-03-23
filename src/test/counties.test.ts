d6de8f19d207ea3fadf3a5d3dc529094a4781504
kihuha <njihiadarius@gmail.com>
2021-03-23 08:34:26 +0300

    Remove stale test

--- src/test/counties.test.ts
+++
@@ -1,9 +0,0 @@
-import request from "supertest"
-import app from "../app"
-
-test("Should fetch specific county data", async () => {
-  // lowercase
-  const res = await request(app).get("/api/v1/counties/nairobi").expect(200)
-
-  expect(res.body).toMatchObject({})
-})
